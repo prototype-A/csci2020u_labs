b574319dd0c43591d224fd13671cdca022e33491
Alex Zheng <Alexzheng123@hotmail.com>
2018-02-05 12:53:24 -0500

    Create working "registration form"

---
+++ lab04/src/main/java/csci2020u/lab04/Main.java
@@ -0,0 +1,100 @@
+package csci2020u.lab04;
+
+import javafx.application.Application;
+import javafx.event.*;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+	private TextField usernameField;
+	private PasswordField passwordField;
+	private TextField fullNameField;
+	private TextField emailField;
+	private TextField phoneNumField;
+	private DatePicker birthDatePicker;
+	private Button registerButton;
+
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		// Application window title
+		primaryStage.setTitle("Lab 04");
+
+		// Initialize the GridPane
+		GridPane gp = new GridPane();
+		gp.setPadding(new Insets(10, 10, 10, 10));
+		gp.setHgap(10);
+		gp.setVgap(10);
+
+		// Initialize input fields
+		usernameField = new TextField();
+		passwordField = new PasswordField();
+		fullNameField = new TextField();
+		emailField = new TextField();
+		phoneNumField = new TextField();
+		birthDatePicker = new DatePicker();
+
+		// Register button
+		registerButton = new Button("Register");
+		registerButton.setDefaultButton(true);
+		registerButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				// Print results
+				System.out.println("\nNew Registrant: " + 
+									"\nUsername: " + usernameField.getText() + 
+									"\nPassword: " + passwordField.getText() + 
+									"\nFull Name: " + fullNameField.getText() + 
+									"\nE-Mail: " + emailField.getText() + 
+									"\nPhone #: " + phoneNumField.getText() + 
+									"\nDate of Birth: " + 
+									birthDatePicker.getEditor().getText());
+
+				// Clear input fields
+				usernameField.clear();
+				passwordField.clear();
+				fullNameField.clear();
+				emailField.clear();
+				phoneNumField.clear();
+			}
+		});
+
+		// Add Labels
+		gp.add(new Label("Username: "), 0, 0);
+		gp.add(new Label("Password: "), 0, 1);
+		gp.add(new Label("Full Name: "), 0, 2);
+		gp.add(new Label("E-mail: "), 0, 3);
+		gp.add(new Label("Phone #: "), 0, 4);
+		gp.add(new Label("Birth Date: "), 0, 5);
+
+		// Add input fields
+		gp.add(usernameField, 1, 0);
+		gp.add(passwordField, 1, 1);
+		gp.add(fullNameField, 1, 2);
+		gp.add(emailField, 1, 3);
+		gp.add(phoneNumField, 1, 4);
+		gp.add(birthDatePicker, 1, 5);
+
+		// Add register button
+		gp.add(registerButton, 1, 6);
+
+		// Create the application scene
+		Scene scene = new Scene(gp, 600, 400);
+
+		// Set the application scene
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+
+}
