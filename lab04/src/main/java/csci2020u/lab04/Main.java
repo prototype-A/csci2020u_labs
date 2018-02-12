b23cbce66fa07484e02f32b7a716ef599fa8fbc2
Alex Zheng <Alexzheng123@hotmail.com>
2018-02-12 09:13:14 -0500

    Integrate apache commons-validator

--- lab04/src/main/java/csci2020u/lab04/Main.java
+++ lab04/src/main/java/csci2020u/lab04/Main.java
@@ -1,11 +1,14 @@
 package csci2020u.lab04;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 import javafx.application.Application;
 import javafx.event.*;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -15,6 +18,7 @@ public class Main extends Application {
 	private PasswordField passwordField;
 	private TextField fullNameField;
 	private TextField emailField;
+	private Label invalidEmailLabel;
 	private TextField phoneNumField;
 	private DatePicker birthDatePicker;
 	private Button registerButton;
@@ -44,28 +48,39 @@ public class Main extends Application {
 		phoneNumField = new TextField();
 		birthDatePicker = new DatePicker();
 
+		// Initialize error labels
+		invalidEmailLabel = new Label();
+
 		// Register button
 		registerButton = new Button("Register");
 		registerButton.setDefaultButton(true);
 		registerButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				// Print results
-				System.out.println("\nNew Registrant: " + 
-									"\nUsername: " + usernameField.getText() + 
-									"\nPassword: " + passwordField.getText() + 
-									"\nFull Name: " + fullNameField.getText() + 
-									"\nE-Mail: " + emailField.getText() + 
-									"\nPhone #: " + phoneNumField.getText() + 
-									"\nDate of Birth: " + 
-									birthDatePicker.getEditor().getText());
+				if (!EmailValidator.getInstance(true).isValid(emailField.getText())) {
+					// Validate email and phone # input
+					invalidEmailLabel.setText("Invalid E-Mail Address");
+					invalidEmailLabel.setTextFill(new Color(1, 0, 0, 1.0));
+				} else {
+					// Print results
+					System.out.println("\nNew Registrant: " + 
+										"\nUsername: " + usernameField.getText() + 
+										"\nPassword: " + passwordField.getText() + 
+										"\nFull Name: " + fullNameField.getText() + 
+										"\nE-Mail: " + emailField.getText() + 
+										"\nPhone #: " + phoneNumField.getText() + 
+										"\nDate of Birth: " + 
+										birthDatePicker.getEditor().getText());
 
-				// Clear input fields
-				usernameField.clear();
-				passwordField.clear();
-				fullNameField.clear();
-				emailField.clear();
-				phoneNumField.clear();
+					// Clear input fields
+					usernameField.clear();
+					passwordField.clear();
+					fullNameField.clear();
+					emailField.clear();
+					invalidEmailLabel.setText("");
+					phoneNumField.clear();
+					birthDatePicker.getEditor().clear();
+				}
 			}
 		});
 
@@ -76,6 +91,7 @@ public class Main extends Application {
 		gp.add(new Label("E-mail: "), 0, 3);
 		gp.add(new Label("Phone #: "), 0, 4);
 		gp.add(new Label("Birth Date: "), 0, 5);
+		gp.add(invalidEmailLabel, 2, 3);
 
 		// Add input fields
 		gp.add(usernameField, 1, 0);
@@ -94,7 +110,6 @@ public class Main extends Application {
 		// Set the application scene
 		primaryStage.setScene(scene);
 		primaryStage.show();
-
 	}
 
 }
