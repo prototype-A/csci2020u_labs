36abf9636cf21c3a09677e30b7392c96b5285aa6
Alex Zheng <Alexzheng123@hotmail.com>
2018-02-22 15:11:36 -0500

    Complete lab 5 extra challence

--- lab05/src/main/java/csci2020u/lab05/Main.java
+++ lab05/src/main/java/csci2020u/lab05/Main.java
@@ -1,9 +1,13 @@
 package csci2020u.lab05;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -53,8 +57,66 @@ public class Main extends Application {
 		studentTable.getColumns().setAll(sidCol, asmtsMarkCol, midtermMarkCol, 
 								finalExamMarkCol, finalMarkCol, letterGradeCol);
 
+
+		// Add student record section
+		GridPane addSection = new GridPane();
+		addSection.setPadding(new Insets(10, 10, 10, 10));
+		addSection.setVgap(10);
+		addSection.setHgap(10);
+		
+		Label sidLabel = new Label("SID:");
+		addSection.add(sidLabel, 0, 0);
+		TextField sidField = new TextField();
+		sidField.setPromptText("Student ID");
+		addSection.add(sidField, 1, 0);
+
+		Label asmtsMarkLabel = new Label("Assignments:");
+		addSection.add(asmtsMarkLabel, 2, 0);
+		TextField asmtsMarkField = new TextField();
+		asmtsMarkField.setPromptText("Assignments Mark/100");
+		addSection.add(asmtsMarkField, 3, 0);
+
+		Label midtermMarkLabel = new Label("Midterm:");
+		addSection.add(midtermMarkLabel, 0, 1);
+		TextField midtermMarkField = new TextField();
+		midtermMarkField.setPromptText("Midterm Mark/100");
+		addSection.add(midtermMarkField, 1, 1);
+
+		Label examMarkLabel = new Label("Final Exam:");
+		addSection.add(examMarkLabel, 2, 1);
+		TextField examMarkField = new TextField();
+		examMarkField.setPromptText("Final Exam Mark/100");
+		addSection.add(examMarkField, 3, 1);
+
+		// Add new student record button
+		Button addButton = new Button("Add");
+		addButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				// Get field information
+				String sid = sidField.getText();
+				float asmtsMark = Float.parseFloat(asmtsMarkField.getText());
+				float midtermMark = Float.parseFloat(midtermMarkField.getText());
+				float examMark = Float.parseFloat(examMarkField.getText());
+				
+				StudentRecord newStudentRecord = new StudentRecord(sid, asmtsMark, midtermMark, examMark);
+
+				// Add student record info to table
+				studentTable.getItems().add(newStudentRecord);
+
+				// Clear the fields
+				sidField.setText("");
+				midtermMarkField.setText("");
+				asmtsMarkField.setText("");
+				examMarkField.setText("");
+			}
+		});
+		addSection.add(addButton, 1, 4);
+
+
 		BorderPane layout = new BorderPane();
 		layout.setCenter(studentTable);
+		layout.setBottom(addSection);
 
 		// Create the application scene
 		Scene scene = new Scene(layout, 800, 600);
